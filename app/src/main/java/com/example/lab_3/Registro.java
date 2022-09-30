d25900103cb20fa4ac4cbdca31a47c96dc6c9388
pipipi <guillen.nicole@pucp.edu.pe>
2022-09-30 08:34:42 -0500

    principal

--- app/src/main/java/com/example/lab_3/Registro.java
+++ app/src/main/java/com/example/lab_3/Registro.java
@@ -6,17 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Registro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registro);
     }
-    public void irRegistro(View view){
-        Intent intent = new Intent(MainActivity.this, Registro.class);
-        startActivityForResult(intent, 1);
-    }
-
 
 }
